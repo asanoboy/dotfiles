69d327c7fe6fecd1f6dea8e275b0226a8f801db7
Yosuke Asano <asano@talentio.com>
2016-02-03 16:12:29 +0900

    Install ack.

--- init.sh
+++ init.sh
@@ -4,3 +4,6 @@ cd wk
 curl https://raw.githubusercontent.com/Shougo/neobundle.vim/master/bin/install.sh > install.sh
 sh install.sh
 rm install.sh
+
+test -f ~/bin || mkdir -p ~/bin
+curl http://beyondgrep.com/ack-2.14-single-file > ~/bin/ack && chmod 0755 !#:3
