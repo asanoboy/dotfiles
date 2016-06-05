e16d777f982e4a979e7b92e5ac385cff42fdb3ec
Yosuke Asano <asano@talentio.com>
2016-06-05 10:30:04 +0900

    Change osascript for iTerm2.

--- .bin/ssh-host-color.sh
+++ .bin/ssh-host-color.sh
@@ -8,8 +8,8 @@ set_term_bgcolor(){
   local G=$2
   local B=$3
   /usr/bin/osascript <<EOF
-tell application "iTerm"
-  tell the current terminal
+tell application "iTerm2"
+  tell the current window
     tell the current session
       set background color to {$(($R*65535/255)), $(($G*65535/255)), $(($B*65535/255))}
     end tell
